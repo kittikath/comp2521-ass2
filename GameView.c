0aab6e1d8a273516fd2553a91a1a5ff2d0308e5b
Jon Luo <z5209823@ad.unsw.edu.au>
2020-07-23 15:12:35 +1000

    Fixed styling issues

--- GameView.c
+++ GameView.c
@@ -5,7 +5,7 @@
 // 2014-07-01	v1.0	Team Dracula <cs2521@cse.unsw.edu.au>
 // 2017-12-01	v1.1	Team Dracula <cs2521@cse.unsw.edu.au>
 // 2018-12-31	v2.0	Team Dracula <cs2521@cse.unsw.edu.au>
-// 2020-07-10   v3.0    Team Dracula <cs2521@cse.unsw.edu.au>
+// 2020-07-10  v3.0  Team Dracula <cs2521@cse.unsw.edu.au>
 //
 ////////////////////////////////////////////////////////////////////////
 
@@ -25,7 +25,7 @@
 
 typedef struct playerView {
 	Player player;
-    int health;
+   int health;
 	int score;
 } PlayerView;
 
@@ -51,7 +51,7 @@ GameView GvNew(char *pastPlays, Message messages[])
 		fprintf(stderr, "Couldn't allocate GameView!\n");
 		exit(EXIT_FAILURE);
 	}
-	
+   
 	new->score = GAME_START_SCORE;
 	new->round = (strlen(pastPlays)+1)/40;
 	printf("round %d\n", new->round);
