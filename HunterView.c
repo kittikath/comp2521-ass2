da54a3d50db90640e336d2dfc8f5594a8010c9d0
kath <ikath@hotmail.com>
2020-07-30 00:29:51 +1000

    working on shortest path

--- HunterView.c
+++ HunterView.c
@@ -19,13 +19,34 @@
 #include "HunterView.h"
 #include "Map.h"
 #include "Places.h"
+#include "Queue.h"
 // add your own #includes here
-
 // TODO: ADD YOUR OWN STRUCTS HERE
 
 struct hunterView {
 	GameView gameView;
+	char *pastPlays;
+	Message *messages;
 };
+
+// helper functions
+
+char *getLastMove(GameView gv, Player player);
+char* getPlayerMove(char *pastPlays, Player player, Round round);
+
+bool placeMatch(char *pastPlays, Player player, PlaceId Place,
+                Round roundStart, Round roundEnd);
+Round placeBeenF(char *pastPlays, Player player, PlaceId place);
+Round placeBeenL(char *pastPlays, Player player, PlaceId place);
+
+int updateHunterHealth(char *move, int health, PlaceId prevLoc, PlaceId currLoc);
+int updateDraculaHealth(GameView gv, char *move, int health);
+int hunterDeathCount(GameView gv);
+
+PlaceId *playerMoveHistory(GameView gv, Player player, int *numReturnedMoves);
+PlaceId *playerLastMoves(GameView gv, Player player, int numMoves, 
+                         int *numReturnedMoves);
+void findDraculaLocation(int numMoves, PlaceId *draculaMoves);
 
 ////////////////////////////////////////////////////////////////////////
 // Constructor/Destructor
@@ -39,12 +60,15 @@ HunterView HvNew(char *pastPlays, Message messages[])
 		exit(EXIT_FAILURE);
 	}
 
+	new->pastPlays = pastPlays;
+	new->messages = messages;
+	new->gameView = GvNew(pastPlays, messages);
 	return new;
 }
 
 void HvFree(HunterView hv)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	// TODO: DONE
 	free(hv);
 }
 
@@ -53,38 +77,38 @@ void HvFree(HunterView hv)
 
 Round HvGetRound(HunterView hv)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	// TODO: DONE
 	return GvGetRound(hv->gameView);
 }
 
 Player HvGetPlayer(HunterView hv)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	// TODO: DONE
 	return GvGetPlayer(hv->gameView);
 }
 
 int HvGetScore(HunterView hv)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	// TODO: DONE
 	return GvGetScore(hv->gameView);
 }
 
 int HvGetHealth(HunterView hv, Player player)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	// TODO: DONE
 	return GvGetHealth(hv->gameView, player);
 }
 
 PlaceId HvGetPlayerLocation(HunterView hv, Player player)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return NOWHERE;
+	// TODO: DONE
+	return GvGetPlayerLocation(hv->gameView, player);
 }
 
 PlaceId HvGetVampireLocation(HunterView hv)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return NOWHERE;
+	// TODO: DONE
+	return GvGetVampireLocation(hv->gameView);
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -92,9 +116,26 @@ PlaceId HvGetVampireLocation(HunterView hv)
 
 PlaceId HvGetLastKnownDraculaLocation(HunterView hv, Round *round)
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	// int numLocs = GvGetLastLocations(hv->gameView, PLAYER_DRACULA, round, &round, &true);
-	return NOWHERE;
+	// TODO: DONE
+	int numReturnedLocs = 0;
+	bool canFree = true;
+	PlaceId *draculaLocation = GvGetLocationHistory(hv->gameView ,PLAYER_DRACULA , &numReturnedLocs, &canFree);
+	int known = numReturnedLocs-1;
+	while(known > 0){
+		if(placeIsReal(draculaLocation[known])){
+			break;
+		}
+		known--;
+	}
+	if(!placeIsReal(draculaLocation[known])){
+		return NOWHERE;
+	}
+
+	Round latestRound = placeBeenL(hv->pastPlays, PLAYER_DRACULA, draculaLocation[known]);
+	*round = latestRound;
+	PlaceId draculaPlace = draculaLocation[known];
+    free(draculaLocation); 
+	return draculaPlace;
 	
 }
 
@@ -102,8 +143,42 @@ PlaceId *HvGetShortestPathTo(HunterView hv, Player hunter, PlaceId dest,
                              int *pathLength)
 {
 	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	
 	*pathLength = 0;
+	PlaceId curr;
+	Map m = MapNew();
+	PlaceId src = HvGetPlayerLocation(hv, hunter);
+	int pred[m->nV];
+	//set all to -1 (not found)
+	for(int i = 0; i < m->nV; i++){
+		pred[i] = -1;
+	}
+	Queue Q = newQueue();
+	QueueJoin(Q, src);
+	while(!QueueIsEmpty(Q)){
+		curr = QueueLeave(Q);
+		//check for connections of node
+		for(int j = 0; j < m->nV; j++){
+			//we found a connection :O
+			if(m->edges[curr][j] != 0){
+			//add 
+			printf("value %d\n", m->edges[curr][j] != 0);
+				if(pred[j] == -1){
+					pred[j] = curr;
+					QueueJoin(Q, j);
+					showQueue(Q);
+					printf("----------\n");
+				}
+			}
+		}
+	}
+	dropQueue(Q);
+	//no path found
+	if(pred[dest] == -1){
+		return NULL;
+	}
+	
+
+
 	return NULL;
 }
 
